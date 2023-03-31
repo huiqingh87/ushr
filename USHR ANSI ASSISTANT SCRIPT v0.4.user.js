25540b5685bccf7481fdf100432a1a3bb665bec9
huiqingh87 <huiqingh87@gmail.com>
2023-03-31 10:48:35 -0400

    Update USHR ANSI ASSISTANT SCRIPT v0.4.user.js

--- USHR ANSI ASSISTANT SCRIPT v0.4.user.js
+++ USHR ANSI ASSISTANT SCRIPT v0.4.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         USHR ANSI ASSISTANT SCRIPT
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://mmp.ushrauto.com/
